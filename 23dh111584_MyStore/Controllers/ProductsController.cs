9ec427bd30acb6fa73941ae6503f66f4619adb1c
user <user@DESKTOP-HGTSJGP>
2024-10-15 15:16:01 +0700

    Add project files.

---
+++ 23dh111584_MyStore/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23dh111584_MyStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 23dh111584_MyStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23dh111584_MyStore.MvcApplication" Language="C#" %>
